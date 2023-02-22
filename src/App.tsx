a14b2d8f734516f1b1ccfc66ea62557c19ac9f40
iwosmartbear <124812026+iwosmartbear@users.noreply.github.com>
2023-02-22 16:18:30 +0100

    Add fetchTypes fetchFunction and get list into App state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
+import {ToDoObject} from "./types/fetchTypes";
+import {fetchToAPI} from "./utils/functions";
 
 function App() {
+    const [listOfToDos, setListOfToDos]= useState<Promise<ToDoObject>| Promise<ToDoObject[]> | ToDoObject[] | ToDoObject>([]);
+
+    useEffect(()=>{
+        const doFetch = async ()=>{
+            try {
+                const data = await fetchToAPI("GET", '/all') as ToDoObject[];
+                return data;
+            } catch (e){
+                throw new Error('Messed Up')
+            }
+        }
+        doFetch().then(data=>setListOfToDos(data));
+    }, []);
+
+
   return (
     <div className="App">
-        <Header />
+            {[listOfToDos].length === 0 ?
+                <p>Nothing to show!</p> :
+                <Header list={listOfToDos as ToDoObject[]}/>}
+
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ src/types/fetchTypes.ts
@@ -0,0 +1,18 @@
+export interface headerAndBodyObject {
+    [key: string]: number|string|object;
+}
+
+export interface ToDoDTO {
+    ownerId: string;
+    taskContent: string;
+    category: string;
+    priority: 1|2|3|4|5;
+    isOpen: 0|1;
+    dueDate: Date;
+}
+
+export interface ToDoObject extends ToDoDTO{
+    id: string;
+    createdAt: Date;
+    closedAt: Date;
+}
