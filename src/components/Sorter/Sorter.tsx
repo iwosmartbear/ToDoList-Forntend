1dd3f183d3e7f24ba052d60bff9e469262ffcec0
iwosmartbear <124812026+iwosmartbear@users.noreply.github.com>
2023-03-02 08:24:48 +0100

    Refactor Sorter.tsx

--- src/components/Sorter/Sorter.tsx
+++ src/components/Sorter/Sorter.tsx
@@ -1,10 +1,10 @@
-import {Button} from "../common/Button/Button";
-
-import './Sorter.css'
 import {useContext} from "react";
+import {Button} from "../common/Button/Button";
 import {ToDoListContext} from "../../context/ToDoListContextProvider";
 import {SortBy} from "../../utils/sortFunctions";
 import {ToDoObject} from "../../types/fetchTypes";
+
+import './Sorter.css'
 
 export const Sorter = () => {
     const {direction, setSortBy, setDirection, sortListOfToDos, listOfToDos} = useContext(ToDoListContext);
@@ -16,12 +16,25 @@ export const Sorter = () => {
     }
 
     return <div className="sorter">
-        <Button text="A-Z" type="button" className="sort sorting-a-z" func={() => handleSortingClick("taskContent")}/>
-        <Button text="Category" type="button" className="sort sorting-category"
-                func={() => handleSortingClick("category")}/>
-        <Button text="Priority" type="button" className="sort sorting-priority"
-                func={() => handleSortingClick("priority")}/>
-        <Button text="Due Date" type="button" className="sort sorting-dueDate"
-                func={() => handleSortingClick("dueDate")}/>
+        <Button text="A-Z"
+                type="button"
+                className="sort sorting-a-z"
+                func={() => handleSortingClick("taskContent")}
+        />
+        <Button text="Category"
+                type="button"
+                className="sort sorting-category"
+                func={() => handleSortingClick("category")}
+        />
+        <Button text="Priority"
+                type="button"
+                className="sort sorting-priority"
+                func={() => handleSortingClick("priority")}
+        />
+        <Button text="Due Date"
+                type="button"
+                className="sort sorting-dueDate"
+                func={() => handleSortingClick("dueDate")}
+        />
     </div>
 }
